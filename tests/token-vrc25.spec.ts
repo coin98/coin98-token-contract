afdb62ed1c6cc8f0275a8e8c46d2cc559095ab7b
Hieuvo <hieuvo@Hieuvo.local>
2023-07-06 13:52:21 +0700

    write test for new C98 token

--- tests/token-vrc25.spec.ts
+++ tests/token-vrc25.spec.ts
@@ -1,25 +1,33 @@
 // Import the necessary dependencies
 import { ethers } from "hardhat";
-import { Signer } from "ethers";
+import { Signer, BigNumber } from "ethers";
 import { expect } from "chai";
 import { Coin98VRC25 } from "../typechain-types";
 import { ZERO_ADDRESS } from '@coin98/solidity-support-library';
+import { calculateFee } from "./common/fee";
 
 describe("Coin98VRC25 token", async function() {
   let owner: Signer;
   let ownerAddress: string;
+  let sender: Signer;
+  let senderAddress: string;
   let recipient: Signer;
   let recipientAddress: string;
   let c98Token: Coin98VRC25;
   let maxSupply = 1_000_000_000;
+  let minFee = ethers.utils.parseEther("1");
+  let priceN = BigNumber.from("1");
+  let priceD = BigNumber.from("100"); 
 
   beforeEach(async function() {
-    [owner, recipient] = await ethers.getSigners();
+    [owner, sender, recipient] = await ethers.getSigners();
     ownerAddress = await owner.getAddress();
+    senderAddress = await sender.getAddress();
     recipientAddress = await recipient.getAddress();
     const tokenFactory = await ethers.getContractFactory("Coin98VRC25");
     c98Token = await tokenFactory.connect(owner).deploy();
-    await c98Token.mint(ownerAddress, ethers.utils.parseEther("1000"));
+    await c98Token.setFee(priceN, priceD, minFee);
+    await c98Token.mint(senderAddress, ethers.utils.parseEther("1000"));
   });
 
   it("check ownership", async function() {
@@ -48,11 +56,11 @@ describe("Coin98VRC25 token", async function() {
   });
 
   it("should burn tokens", async function() {
-    const amount = ethers.utils.parseEther("1000");
-    const balanceBefore = await c98Token.balanceOf(ownerAddress);
-    await c98Token.connect(owner).burn(amount);
-    const balanceAfter = await c98Token.balanceOf(ownerAddress);
-    expect(balanceAfter).to.equal(balanceBefore.sub(amount));
+    const amount = ethers.utils.parseEther("100");
+    const balanceBefore = await c98Token.balanceOf(senderAddress);
+    await c98Token.connect(sender).burn(amount);
+    const balanceAfter = await c98Token.balanceOf(senderAddress);
+    expect(balanceAfter).to.equal(balanceBefore.sub(amount.add(minFee)));
   });
 
   it("cannot burn exceeds balance", async function() {
@@ -62,55 +70,57 @@ describe("Coin98VRC25 token", async function() {
   });
 
   it("should burnFrom tokens", async function() {
-    const amount = ethers.utils.parseEther("1000");
-    const balanceBefore = await c98Token.balanceOf(ownerAddress);
-    await c98Token.connect(owner).approve(recipientAddress, amount);
-    await c98Token.connect(recipient).burnFrom(ownerAddress, amount);
-    const balanceAfter = await c98Token.balanceOf(ownerAddress);
-    expect(balanceAfter).to.equal(balanceBefore.sub(amount));
+    const amount = ethers.utils.parseEther("100");
+    const balanceBefore = await c98Token.balanceOf(senderAddress);
+    await c98Token.connect(sender).approve(recipientAddress, amount.add(minFee));
+    await c98Token.connect(recipient).burnFrom(senderAddress, amount);
+    const balanceAfter = await c98Token.balanceOf(senderAddress);
+    const totalAmountLeft = amount.add(minFee).add(minFee); //2 times lost fee (approve, burnFrom)
+    expect(balanceAfter).to.equal(balanceBefore.sub(totalAmountLeft));
   });
 
   it("cannot burnFrom exceeds allowance", async function() {
-    const approveAmount = ethers.utils.parseEther("1000");
-    const burnAmount = ethers.utils.parseEther("1001");
-    await c98Token.connect(owner).approve(recipientAddress, approveAmount);
+    const approveAmount = ethers.utils.parseEther("100");
+    const burnAmount = ethers.utils.parseEther("101");
+    await c98Token.connect(sender).approve(recipientAddress, approveAmount);
     await expect(c98Token.connect(recipient).burnFrom(ownerAddress, burnAmount))
     .to.be.rejectedWith("ERC20: burn amount exceeds allowance");
   });
 
   it("should transfer tokens", async function() {
-    const initialOwnerBalance = ethers.utils.parseEther("1000");
-    const initialRecipientBalance = ethers.BigNumber.from(0);
-    const ownerBalance = await c98Token.balanceOf(ownerAddress);
-    const recipientBalance = await c98Token.balanceOf(recipientAddress);
-    expect(ownerBalance).to.equal(initialOwnerBalance);
-    expect(recipientBalance).to.equal(initialRecipientBalance);
+    const initialSenderBalance = ethers.utils.parseEther("1000");
     const transferAmount = ethers.utils.parseEther("500");
-    await c98Token.connect(owner).transfer(recipientAddress, transferAmount);
-    expect(await c98Token.balanceOf(ownerAddress)).to.equal(initialOwnerBalance.sub(transferAmount));
+    const fee = calculateFee(transferAmount, priceN, priceD, minFee);
+    const initialRecipientBalance = ethers.BigNumber.from(0);
+    const ownerBalance = await c98Token.balanceOf(senderAddress);
+    const recipientBalance = await c98Token.balanceOf(recipientAddress);
+    expect(ownerBalance).to.equal(initialSenderBalance);
+    expect(recipientBalance).to.equal(initialRecipientBalance);
+    await c98Token.connect(sender).transfer(recipientAddress, transferAmount);
+    expect(await c98Token.balanceOf(senderAddress)).to.equal(initialSenderBalance.sub(transferAmount.add(fee)));
     expect(await c98Token.balanceOf(recipientAddress)).to.equal(initialRecipientBalance.add(transferAmount));
   });
 
   it("cannot transfer exceeds balance", async function() {
     const amount = ethers.utils.parseEther("1001");
-    await expect(c98Token.connect(owner).transfer(recipientAddress, amount))
+    await expect(c98Token.connect(sender).transfer(recipientAddress, amount))
     .to.be.rejectedWith("ERC20: transfer amount exceeds balance");
   });
 
   it("cannot transfer to the zero address", async function() {
     recipientAddress = ZERO_ADDRESS;
     const transferAmount = ethers.utils.parseEther("500");
-    await expect(c98Token.connect(owner).transfer(recipientAddress, transferAmount))
+    await expect(c98Token.connect(sender).transfer(recipientAddress, transferAmount))
     .to.be.rejectedWith("ERC20: transfer to the zero address");
   });
 
   it("cannot transfer tokens while the token is frozen", async function() {
     const transferAmount = ethers.utils.parseEther("500");
     await c98Token.connect(owner).freeze();
-    await expect(c98Token.connect(owner).transfer(recipientAddress, transferAmount))
+    await expect(c98Token.connect(sender).transfer(recipientAddress, transferAmount))
     .to.be.rejectedWith("ERC20: token transfer while frozen");
     await c98Token.connect(owner).unfreeze();
-    await c98Token.connect(owner).transfer(recipientAddress, transferAmount);
+    await c98Token.connect(sender).transfer(recipientAddress, transferAmount);
   });
 
   it("should rescue accidentally sent token", async function() {
@@ -124,10 +134,11 @@ describe("Coin98VRC25 token", async function() {
   });
 
   it("should approve tokens", async function() {
-    const beforeAllowance = await c98Token.allowance(ownerAddress, recipientAddress);
+    const beforeAllowance = await c98Token.allowance(senderAddress, recipientAddress);
     const amount = ethers.utils.parseEther("1000");
-    await c98Token.connect(owner).approve(recipientAddress, amount);
-    const afterAllowance = await c98Token.allowance(ownerAddress, recipientAddress);
+    const fee = calculateFee(amount, priceN, priceD, minFee);
+    await c98Token.connect(sender).approve(recipientAddress, amount);
+    const afterAllowance = await c98Token.allowance(senderAddress, recipientAddress);
     expect(afterAllowance).to.equal(beforeAllowance.add(amount));
   });
 
@@ -137,47 +148,48 @@ describe("Coin98VRC25 token", async function() {
   });
   
   it("should increase allowance", async function() {
-    const beforeAllowance = await c98Token.allowance(ownerAddress, recipientAddress);
+    const beforeAllowance = await c98Token.allowance(senderAddress, recipientAddress);
     const initAmount = ethers.utils.parseEther("1000");
-    await c98Token.connect(owner).approve(recipientAddress, initAmount);
+    await c98Token.connect(sender).approve(recipientAddress, initAmount);
     const increaseAmount = ethers.utils.parseEther("500");
-    await c98Token.increaseAllowance(recipientAddress, increaseAmount);
-    const afterAllowance = await c98Token.allowance(ownerAddress, recipientAddress);
+    await c98Token.connect(sender).increaseAllowance(recipientAddress, increaseAmount);
+    const afterAllowance = await c98Token.allowance(senderAddress, recipientAddress);
     expect(afterAllowance).to.equal(beforeAllowance.add(initAmount).add(increaseAmount));
   });
 
   it("should decrease allowance", async function() {
-    const beforeAllowance = await c98Token.allowance(ownerAddress, recipientAddress);
+    const beforeAllowance = await c98Token.allowance(senderAddress, recipientAddress);
     const initAmount = ethers.utils.parseEther("1000");
-    await c98Token.connect(owner).approve(recipientAddress, initAmount);
+    await c98Token.connect(sender).approve(recipientAddress, initAmount);
     const decreaseAmount = ethers.utils.parseEther("500");
-    await c98Token.decreaseAllowance(recipientAddress, decreaseAmount);
-    const afterAllowance = await c98Token.allowance(ownerAddress, recipientAddress);
+    await c98Token.connect(sender).decreaseAllowance(recipientAddress, decreaseAmount);
+    const afterAllowance = await c98Token.allowance(senderAddress, recipientAddress);
     expect(afterAllowance).to.equal(beforeAllowance.add(initAmount).sub(decreaseAmount));
   });
 
   it("cannot decrease allowance below zero", async function() {
     const initAmount = ethers.utils.parseEther("1000");
-    await c98Token.connect(owner).approve(recipientAddress, initAmount);
+    await c98Token.connect(sender).approve(recipientAddress, initAmount);
     const decreaseAmount = ethers.utils.parseEther("1001");
-    await expect(c98Token.decreaseAllowance(recipientAddress, decreaseAmount))
+    await expect(c98Token.connect(sender).decreaseAllowance(recipientAddress, decreaseAmount))
     .to.be.rejectedWith("ERC20: decreased allowance below zero");
   });
 
   it("should transferFrom successful", async function() {
     const balanceBefore = await c98Token.balanceOf(recipientAddress);
-    const amount = ethers.utils.parseEther("1000");
-    await c98Token.connect(owner).approve(recipientAddress, amount);
-    await c98Token.connect(recipient).transferFrom(ownerAddress, recipientAddress, amount);
+    const amount = ethers.utils.parseEther("100");
+    const fee = calculateFee(amount, priceN, priceD, minFee);
+    await c98Token.connect(sender).approve(recipientAddress, amount.add(fee));
+    await c98Token.connect(recipient).transferFrom(senderAddress, recipientAddress, amount);
     const balanceAfter = await c98Token.balanceOf(recipientAddress);
     expect(balanceAfter).to.equal(balanceBefore.add(amount));
   });
 
   it("cannot transferFrom exceeds allowance", async function() {
-    const approveAmount = ethers.utils.parseEther("999");
-    const transferAmount = ethers.utils.parseEther("1000");
-    await c98Token.connect(owner).approve(recipientAddress, approveAmount);
-    await expect(c98Token.connect(recipient).transferFrom(ownerAddress, recipientAddress, transferAmount))
+    const approveAmount = ethers.utils.parseEther("100");
+    const transferAmount = ethers.utils.parseEther("101");
+    await c98Token.connect(sender).approve(recipientAddress, approveAmount);
+    await expect(c98Token.connect(recipient).transferFrom(senderAddress, recipientAddress, transferAmount))
     .to.be.rejectedWith("ERC20: transfer amount exceeds allowance");
   });
 });
